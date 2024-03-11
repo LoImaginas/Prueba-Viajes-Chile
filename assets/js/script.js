ef09141842264f569c13c89c35f591887aa8284e
Lolett Llanquinao <lmarialolett@yahoo.es>
2024-03-11 13:37:10 -0300

    actualiza

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+   $("h4").click(function () {
+      $("h4").addClass("text-danger");
+   
+   });
+
+   $("h4").click(function () {
+      $("h4").addClass("text-danger");
+   
+   });
+
+   $(".btn").click(function () {
+      $(".btn").addClass("text-danger");
+   
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+});
